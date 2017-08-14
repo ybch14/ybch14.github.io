97fece8ea5c0776b626715a9c1bcffbff1052b01
ybch14 <ybch14@gmail.com>
2017-08-14 12:12:02 +0800

    adjust toc text indent

--- js/toc.js
+++ js/toc.js
@@ -19,7 +19,7 @@
 
     function createLink (header) {
       var innerText = (header.textContent === undefined) ? header.innerText : header.textContent;
-      return "<a href='#" + fixedEncodeURIComponent(header.id) + "'>" + innerText + "</a>";
+      return "<a href='#" + fixedEncodeURIComponent(header.id) + "'><p>" + innerText + "</p></a>";
     }
 
     var headers = $(settings.headers).filter(function() {
